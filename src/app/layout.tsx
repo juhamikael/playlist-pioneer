c61322d2ebaa6038598e27448aa2964994d7a473
juhamikael <dev.juhamikael@gmail.com>
2023-12-25 03:58:19 +0200

    Modify imports + add functionality to analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css';
-import NavBar from '@/components/layout/NavBar';
+import NavBar from '@/components/global/NavBar';
 import { Toaster } from 'sonner';
 import { Montserrat } from "next/font/google";
+import Footer from '@/components/global/Footer';
+import { cn } from '@/lib/utils';
+import { ReactNode, Suspense } from 'react';
+import { PHProvider, PostHogPageview } from './providers';
+
 const font = Montserrat({ subsets: ["latin"] });
 export default function RootLayout({
   children,
@@ -11,16 +16,22 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en" className={font.className}>
-        <body>
-          <NavBar />
-          {children}
-          <Toaster
-            position='top-center'
-            richColors
-          />
-        </body>
+      <html lang="en" className={cn(font.className, "flex flex-col min-h-screen")}>
+        <Suspense>
+          <PostHogPageview />
+        </Suspense>
+        <PHProvider>
+          <body className="flex-grow">
+            <Toaster
+              position='top-center'
+              richColors
+            />
+            <NavBar />
+            {children}
+            <Footer />
+          </body>
+        </PHProvider>
       </html>
-    </ClerkProvider>
+    </ClerkProvider >
   )
 }
