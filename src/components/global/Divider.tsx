bacae5946aa531787b135a948d21adb6f9b1ec5a
juhamikael <dev.juhamikael@gmail.com>
2023-12-25 03:59:03 +0200

    Update Divider.tsx

--- src/components/global/Divider.tsx
+++ src/components/global/Divider.tsx
@@ -4,7 +4,7 @@ import { FC } from "react";
 interface TDividerProps extends React.HTMLAttributes<HTMLHRElement> { }
 
 const Divider: FC<TDividerProps> = ({ className }) => {
-    return <hr className={cn("my-4 border-t border-card-foreground/20", className)} />;
+    return <hr className={cn("my-4 border-t border-card-foreground/10", className)} />;
 };
 
 export default Divider;
