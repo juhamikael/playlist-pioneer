272106c877090240a3d13c0180fb5c57829389d2
juhamikael <dev.juhamikael@gmail.com>
2023-12-25 03:59:10 +0200

    Create NavBar.tsx

---
+++ src/components/global/NavBar.tsx
@@ -0,0 +1,108 @@
+"use client"
+import { UserButton, currentUser, auth, useUser } from "@clerk/nextjs";
+import CustomSignInButton from "@/components/clerk/CustomSignInButton"
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../global/Logo";
+import CustomLink from "../global/CustomLink";
+
+import { Settings } from "lucide-react"
+import Preferences from "../Preferences";
+import { Menu } from "lucide-react"
+import { useState } from "react";
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+    DrawerFooter,
+} from "@/components/ui/drawer"
+
+const Links = [
+    {
+        label: "About",
+        url: "/about",
+    },
+    {
+        label: "FAQ",
+        url: "/faq",
+    },
+    {
+        label: "Contact",
+        url: "/contact",
+    },
+]
+
+const NavBar = ({ }) => {
+    const { isLoaded, isSignedIn } = useUser();
+
+    const MenuItems = () => {
+        return (
+            <>
+                {Links.map(({ label, url }) => (
+                    <CustomLink href={url} asLink key={label}>
+                        {label}
+                    </CustomLink>
+                ))}
+            </>
+        )
+    };
+    return (
+        <MaxWidthWrapper className="flex justify-between py-4 border-b border-card-foreground/20 relative">
+            <CustomLink href="/" className="group">
+                <Logo hover textSize={"sm"} />
+            </CustomLink>
+
+            <div className="ml-auto flex items-center ">
+                <div className="hidden lg:flex gap-x-6 mx-6 items-center ">
+                    <MenuItems />
+                </div>
+
+                <Drawer>
+                    <DrawerTrigger>
+                        <Menu className="lg:hidden mx-4 hover" size={32} />
+                    </DrawerTrigger>
+                    <DrawerContent>
+                        <DrawerHeader>
+                            <DrawerTitle>
+                                <div className="flex flex-col gap-y-4 my-2 text-left">
+                                    Navigation Menu
+                                    {
+                                        isLoaded && isSignedIn && (
+                                            <CustomLink href="/bookmarks" asLink>
+                                                Bookmarks
+                                            </CustomLink>
+                                        )
+                                    }
+                                    <MenuItems />
+                                </div>
+                            </DrawerTitle>
+
+                        </DrawerHeader>
+
+                    </DrawerContent>
+                </Drawer>
+                {
+                    isLoaded && isSignedIn ? (
+                        <div className="flex items-center gap-x-5">
+                            <CustomLink className="hidden lg:block ml-5" href="/bookmarks" asLink>
+                                Bookmarks
+                            </CustomLink>
+                            <UserButton afterSignOutUrl="/" >
+                                <UserButton.UserProfilePage label="Preferences" url="custom" labelIcon={<Settings className="h-4 w-4" />}>
+                                    <Preferences />
+                                </UserButton.UserProfilePage>
+                            </UserButton>
+                        </div>
+                    ) : (<CustomSignInButton buttonVariant={{ variant: "outline" }} />)
+                }
+            </div>
+
+
+        </MaxWidthWrapper>
+    );
+};
+
+export default NavBar;
