23ae40e33415d9f324c3004336b1aa86aac116d4
juhamikael <dev.juhamikael@gmail.com>
2023-12-25 03:57:38 +0200

    About Page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,38 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { getContentfulData } from "@/lib/contentful/fetchData"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { Document } from '@contentful/rich-text-types';
+import { proseClassName } from "@/styles/prose";
+import { cn } from "@/lib/utils";
+import Divider from "@/components/global/Divider";
+import { Contact } from "@/components/Contact";
+const AboutPage = async ({ }) => {
+    const response = await getContentfulData("aboutUsPage")
+    const data = response.props.data[0];
+    if (!data) {
+        return null
+    }
 
-const AboutPage = ({ }) => {
+    const story = data.fields?.story as Document;
+    const future = data.fields?.future as Document;
+    if (!story || typeof story !== 'object' || !story.nodeType) {
+        return null;
+    }
+
     return (
-        <MaxWidthWrapper>
-            <div>AboutPage</div>
-        </MaxWidthWrapper>
+        <MaxWidthWrapper className={cn("prose", proseClassName, "px-10")}>
+            <div>
+                <h1>About</h1>
+                {documentToReactComponents(story)}
+            </div>
+            <Divider className="my-8" />
+            <div>
+                <h1>{"What's next?"}</h1>
+                {documentToReactComponents(future)}
+            </div>
+            <Divider className="my-8" />
+            <Contact />
+        </MaxWidthWrapper >
     );
 };
 
